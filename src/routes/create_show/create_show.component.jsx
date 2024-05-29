b11b4497d38df9a5f3375ca5aa077ec09ad04777
gandradep <eventsonido@gmail.com>
2024-05-29 21:07:04 +0800

    Started show form. Select Venue, option to create new one with checkbox

--- src/routes/create_show/create_show.component.jsx
+++ src/routes/create_show/create_show.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import ShowForm from '../../components/show-form/show-form.component';
 
 const CreateShow = ({shows}) => {
-  console.log(shows);
+
   return (
     <div >
       <h1>Create new Show</h1>
-
+      <ShowForm shows={shows}/>
     </div>
   );
 }
